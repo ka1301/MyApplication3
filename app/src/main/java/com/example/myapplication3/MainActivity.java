17c5a5c3ef75bdc994aed773df32d565835e36a4
ka1301 <ka1301@outlook.jp>
2023-06-26 13:32:20 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Log.i("テスト","メッセージ");
+//    }
+
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ボタンがクリックされた時の処理
+                textView.setText("Text Changed!");
+                Log.d("onClick","ボタンがクリックされました");
+            }
+        });
+    }
+}
